64469eec5ac2e0f87267e9d6dd359041ddb15a4d
Sreejithks@2000 <sreejithksgupta2255@gmial.com>
2025-01-10 14:17:54 +0530

    added a basic galery

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -12,7 +12,14 @@ export class GalleryComponent {
   galleryimgs: string[] = [
     "assets/gallery/aunty.webp",
     "assets/gallery/boy1.webp",
-    "assets/gallery/class.webp"
+    "assets/gallery/class.webp",
+    "assets/gallery/designer.webp",
+    "assets/gallery/download.webp",
+    "assets/gallery/girl2.webp",
+    "assets/gallery/hero.webp",
+    "assets/gallery/image.webp",
+    "assets/gallery/team1.webp",
+
 ];
   
 
