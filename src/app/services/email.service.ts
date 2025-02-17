82f488f0d6193e36ea524bd471e16449e7e2af3d
sreejith <sreejithksgupta2255@gmail.com>
2025-02-18 04:03:21 +0530

    email prefeerneces can be managed by users

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { UserChoicesService } from './userchoices.service';
+
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 import { Observable } from 'rxjs';
 
@@ -11,7 +13,7 @@ export class EmailService {
   private userId = 'JCFGdn35jddlObtV9';
   // private 2L9rLAQeTZA-SFs44_nC_
   // public JCFGdn35jddlObtV9
-  constructor() {}
+  constructor(private userChoicesService: UserChoicesService) {}
   sendEmail(
     emailtype: string = 'subscription',
     toEmail: string,
@@ -20,36 +22,43 @@ export class EmailService {
     subject: string = "Thank you for subscribing!",
     message: string = "Hello! Thank you for subscribing to learnix. We're excited to have you with us!"
   ): Promise<EmailJSResponseStatus> {
-    const templateParams = {
-      to_name: toName,
-      from_name: fromName,
-      to_email: toEmail,
-      user: toEmail,
-      subject: subject,
-      message: message
-    };
-   if (emailtype === 'otp') {
-     return emailjs.send(this.serviceId, this.templateId, templateParams, this.userId);
+    // Get the user's choices
+    const userChoices = this.userChoicesService.getChoices();
 
-    }
-    // else if (emailtype === 'subscription') {
-    //   return emailjs.send(this.serviceId, this.templateId, templateParams, this.userId);
+    return userChoices.toPromise().then(choices => {
+      // Check if email notifications are enabled
 
-    // } else if (emailtype === 'contactmessage') {
+        const templateParams = {
+          to_name: toName,
+          from_name: fromName,
+          to_email: toEmail,
+          user: toEmail,
+          subject: subject,
+          message: message
+        };
 
-    //   // return emailjs.send(this.serviceId, this.templateId, templateParams, this.userId);
-
-    // } else if (emailtype === 'messagereply') {
-
-    //   return emailjs.send(this.serviceId, this.templateId, templateParams, this.userId);
-    // } else if (emailtype === 'othermsg') {
-
-    //   return emailjs.send(this.serviceId, this.templateId, templateParams, this.userId);
-    // }else {
-    //   console.warn("Invalid email type provided:", emailtype);
-    // }
+        if (emailtype === 'otp' && choices!.receiveEnrollmentNotifications) {
+          return emailjs.send(this.serviceId, this.templateId, templateParams, this.userId);
+        }
+        if (choices!.enableEmailNotifications) {
+        // Send email based on the email type and user's preferences
+       if (emailtype === 'subscription' && choices!.receiveNewsletter) {
+          return emailjs.send(this.serviceId, this.templateId, templateParams, this.userId);
+        } else if (emailtype === 'contactmessage' && choices!.receiveContactMessages) {
+          return emailjs.send(this.serviceId, this.templateId, templateParams, this.userId);
+        } else if (emailtype === 'messagereply' && choices!.receiveMessageReplies) {
+          return emailjs.send(this.serviceId, this.templateId, templateParams, this.userId);
+        } else if (emailtype === 'othermsg' && choices!.receiveOtherMessages) {
+          return emailjs.send(this.serviceId, this.templateId, templateParams, this.userId);
+        } else {
+          console.warn("Email not sent due to user preferences or invalid email type.");
+        }
+      } else {
+        console.warn("Email notifications are disabled.");
+      }
 
       return Promise.resolve({ status: 200, text: "No email sent" } as EmailJSResponseStatus);
+    });
   }
 
 
